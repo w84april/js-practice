ade0aeeac7c1feceb9a5f7e22a8fbf858372646f
ZelenovSS <zelenovss666@gmail.com>
2021-02-15 16:44:26 +0300

    task 2.10

---
+++ main.js
@@ -0,0 +1,12 @@
+let login = "Сотрудник";
+
+let message =
+  login == "Сотрудник"
+    ? "Привет"
+    : login == "Директор"
+    ? "Здравствуйте"
+    : login == ""
+    ? "Нет логина"
+    : "";
+
+console.log(message);
