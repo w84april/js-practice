50a1824c4a1f0308034f760f6a143d94f3137309
ZelenovSS <zelenovss666@gmail.com>
2021-02-16 10:58:26 +0300

    task 2.15

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 //n^k
 
-let n = 2;
+let n = 0;
 let k = 3;
 
 function pow(n, k) {
-  return n ** k;
+  if (n < 1) {
+    return "Введите число больше 0";
+  } else {
+    return n ** k;
+  }
 }
 
 console.log(pow(n, k));
