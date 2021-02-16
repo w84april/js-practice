3fed61bb2fc66ed71dc69229a7841e7bb59ed295
ZelenovSS <zelenovss666@gmail.com>
2021-02-16 14:33:48 +0300

    task 4.4

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-let salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130,
+// Создайте объект calculator (калькулятор) с тремя методами:
+
+//     read() (читать) запрашивает два значения и сохраняет их как свойства объекта.
+//     sum() (суммировать) возвращает сумму сохранённых значений.
+//     mul() (умножить) перемножает сохранённые значения и возвращает результат.
+
+let calculator = {
+  number1: 0,
+  number2: 0,
+  read() {
+    this.number1 = prompt("Введите 1 число");
+    this.number2 = prompt("Введите 2 число");
+  },
+  sum() {
+    return +this.number1 + +this.number2;
+  },
+  mul() {
+    return this.number1 * this.number2;
+  },
 };
-let sum = 0;
 
-for (let key in salaries) {
-  sum = sum + salaries[key];
-}
-
-console.log(sum);
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
