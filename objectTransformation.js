8f174bc0f29202d18ef4337adddd92b4b1d29579
ZelenovSS <zelenovss666@gmail.com>
2021-02-18 10:55:08 +0300

    task 5.5

---
+++ objectTransformation.js
@@ -0,0 +1,19 @@
+// У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
+
+// Напишите код, который создаст ещё один массив объектов с параметрами id и fullName, где fullName – состоит из name и surname.
+
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users = [vasya, petya, masha];
+
+let usersMapped = users.map(function (item) {
+  return { id: item.id, fullName: item.name + " " + item.surname };
+});
+
+console.log(usersMapped);
+
+
+console.log(usersMapped[0].id); // 1
+console.log(usersMapped[0].fullName); // Вася Пупкин
